23d3075864cadff19655cb5034032a42c8304086
Dominic James <30402312+dom3691@users.noreply.github.com>
2024-01-08 22:12:48 +0100

    Added Dependency inj. in program.cs

--- Controllers/PhoneInfoController.cs
+++ Controllers/PhoneInfoController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CountryApp.Domain.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CountryApp.Controllers
--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+using CountryApp.Data;
+using CountryApp.Domain.Interfaces.Data;
+using CountryApp.Domain.Interfaces.Services;
+using CountryApp.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IPhoneInfoService, PhoneInfoService>();
+builder.Services.AddScoped<ICountryService, CountryService>();
+builder.Services.AddScoped<ICountryDetailService, CountryDetailService>();
+builder.Services.AddTransient<ICountryRepository, CountryRepository>();
+builder.Services.AddTransient<ICountryDetailsRepository, CountryDetailRepository>();
+
+
+
 
 var app = builder.Build();
 
